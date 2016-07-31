22db3224671184155c33b137335cb740669fd656
Brutesque <brutesque@users.noreply.github.com>
2016-07-31 06:36:47 +0200

    Update bb-deploy-key.py

--- bb-deploy-key.py
+++ bb-deploy-key.py
@@ -4,8 +4,8 @@ import argparse, requests, urllib, os.path, getpass
 parser = argparse.ArgumentParser(description='Add deployment key to bitbucket repository.')
 parser.add_argument('-a', '--accountname', help='The team or individual account.', type=str, required=True)
 parser.add_argument('-r', '--repo_slug', help="The repo identifier (not to be confused with the repo's name).", type=str, required=True)
-parser.add_argument('-l', '--label', help='A display name for the key.', type=str)
 parser.add_argument('-u', '--user', help='The content of the key.', type=str, required=True)
+parser.add_argument('-l', '--label', help='A display name for the key.', type=str)
 parser.add_argument("file", help="The file containin the public key. (Eg.: ~/.ssh/id_rsa.pub)")
 
 args = parser.parse_args()
