c6b82fd72faddd3c4aefeb87a6a14f457096eea9
siers <wimuan@gmail.com>
2017-07-24 18:13:29 +0300

    Formatting for arg parsing.

--- bb-deploy-key.py
+++ bb-deploy-key.py
@@ -5,19 +5,25 @@ import os
 import getpass
 
 parser = argparse.ArgumentParser(description='Add deployment key to bitbucket repository.')
-parser.add_argument('-a', '--accountname', help='The team or individual account.', type=str, required=True)
-parser.add_argument(
-    '-r',
-    '--repo_slug',
-    help="The repo identifier (not to be confused with the repo's name).",
-    type=str,
-    required=True
-)
-parser.add_argument('-u', '--user', help='Your bitbucket username.', type=str, required=True)
-parser.add_argument('-l', '--label', help='A display name for the key.', type=str)
-parser.add_argument('file', help='The file containing the public key. (Eg.: ~/.ssh/id_rsa.pub)')
-parser.add_argument('-p', '--password', default=os.environ.get('PASSWORD')
-)
+
+parser.add_argument('-a', '--accountname',
+    help='The team or individual account.', type=str, required=True)
+
+parser.add_argument('-r', '--repo_slug', type=str, required=True,
+    help="The repo identifier (not to be confused with the repo's name).")
+
+parser.add_argument('-u', '--user', type=str, required=True,
+    help='Your bitbucket username.')
+
+parser.add_argument('-l', '--label', type=str,
+    help='A display name for the key.')
+
+parser.add_argument('file',
+    help='The file containing the public key. (Eg.: ~/.ssh/id_rsa.pub)')
+
+parser.add_argument('-p', '--password',
+    help='Supply password (insecure, use $BITBUCKET_PASSWORD)',
+    default=os.environ.get('BITBUCKET_PASSWORD'))
 
 args = parser.parse_args()
 password = args.password if args.password else getpass.getpass()
